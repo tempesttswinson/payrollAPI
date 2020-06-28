01f9a5f9d397692fa3e41cfec7c08bcb3e3de998
tempesttswinson <tempestt.swinson@gmail.com>
2020-06-28 08:43:06 -0400

    edit the controller to add the assembler in the constructor

--- src/main/java/com/win/payroll/EmployeeController.java
+++ src/main/java/com/win/payroll/EmployeeController.java
@@ -23,8 +23,12 @@ class EmployeeController {
 
     private final EmployeeRepository repository;
 
-    EmployeeController(EmployeeRepository repository) {
+    private final EmployeeModelAssembler assembler;
+
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
+
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     // Aggregate root
