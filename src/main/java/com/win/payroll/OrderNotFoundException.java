85ae33b8b6ed66e1bd9715b196600f859dac151b
tempesttswinson <tempestt.swinson@gmail.com>
2020-06-28 09:48:45 -0400

    added default serialVersion in the Exception classes to get rid of errors

--- src/main/java/com/win/payroll/OrderNotFoundException.java
+++ src/main/java/com/win/payroll/OrderNotFoundException.java
@@ -2,6 +2,11 @@ package com.win.payroll;
 
 class OrderNotFoundException extends RuntimeException {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
     OrderNotFoundException(Long id) {
         super("Could not find order " + id);
     }
